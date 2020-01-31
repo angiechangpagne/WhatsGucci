148a20afabc59bb3a81e895f8b86b2eb41f9d715
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-01-31 09:46:22 -0800

    what's gucci chat app

---
+++ whatsGucci-server/db.ts
@@ -0,0 +1,60 @@
+export const messages = [
+  {
+    id: '1',
+    content: 'You Gucci Bro?',
+    createdAt: new Date(
+      new Date('1-1-2020').getTime()-60*1000*1000
+    ),
+  },
+  {
+    id: '2',
+    content: 'Nah we Fendi. We got beef',
+    createdAt: new Date(
+      new Date('1-1-2020').getTime()-2*60*1000*1000
+    ),
+  },
+  {
+    id: '3',
+    content: 'Like its poetry in motion',
+    createdAt: new Date(
+      new Date('1-1-2020').getTime()-24*60*1000*1000
+    ),
+  },
+  {
+    id: '4',
+    content: 'This party is wack bruh',
+    createdAt: new Date(
+      new Date('1-1-2020').getTime()-14*24*60*1000*1000
+    ),
+  },
+];
+
+
+
+export const chats = [
+  {
+    id: '1',
+    name: 'Logan Paul',
+    picture: '',
+    lastMessage: '1'
+  },
+  {
+    id: '2',
+    name: 'Jake Paul',
+    picture: '',
+    lastMessage: '2'
+  },
+  {
+    id: '3',
+    name: 'Lil Uzi Vert',
+    picture: '',
+    lastMessage: '3'
+  },
+  {
+    id: '4',
+    name: 'Karl Lagerfeld',
+    picture: '',
+    lastMessage: '4'
+  },
+];
+
